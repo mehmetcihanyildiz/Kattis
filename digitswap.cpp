63cd38e5edc4a8352723d21c121e2e885f0a03f4
Mehmet Cihan Yıldız <129149603+mehmetcihanyildiz@users.noreply.github.com>
2023-06-02 16:27:12 +0300

    Add files via upload

---
+++ digitswap.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    char first = s[0];
+    char second = s[1];
+    
+    cout<<second<<first;
+    
+    return 0;
+}
