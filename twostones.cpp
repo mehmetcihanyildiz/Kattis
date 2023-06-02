033800e5a9aae17884b4dd0cf558203607ca3c3c
Mehmet Cihan Yıldız <129149603+mehmetcihanyildiz@users.noreply.github.com>
2023-06-02 10:10:05 +0300

    Add files via upload

---
+++ twostones.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int stones;
+    
+    cin>>stones;
+    
+    if(stones%2 == 1){
+        cout<<"Alice";;
+    }
+    else{
+        cout<<"Bob";
+    }
+    
+    
+    return 0;
+}
