272bfa4b60d46ca6a5335ee650d4a29524937c0f
Mehmet Cihan Yıldız <129149603+mehmetcihanyildiz@users.noreply.github.com>
2023-06-02 10:14:14 +0300

    Add files via upload

---
+++ jackolanternjuxtaposition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int eye,nose,mouth;
+    int result;
+    
+    cin>>eye>>nose>>mouth;
+    result = eye*nose*mouth;
+    
+    cout<<result;
+    
+    return 0;
+}
